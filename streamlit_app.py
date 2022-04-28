ff3038d0fcb96bc2adc6a5e2e32872a0bfffbe48
solitaIsabelleJo <102659576+solitaIsabelleJo@users.noreply.github.com>
2022-04-28 15:06:09 +0200

    Update streamlit_app.py
    fixed missing paranthesis

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,6 @@ streamlit.header('🍓Build Your Own Fruit Smoothie🍌')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 streamlit.dataframe(my_fruit_list)
