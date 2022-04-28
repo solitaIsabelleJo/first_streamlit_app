db881c8777be9110eb3e48248bae01875746831e
solitaIsabelleJo <102659576+solitaIsabelleJo@users.noreply.github.com>
2022-04-28 15:02:57 +0200

    Update streamlit_app.py
    Imported the my_fruit_list datafram from S3 bucket and displayed with pandas.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
+import pandas
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Parents\' New Healthy Diner')
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
@@ -8,3 +9,6 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍓Build Your Own Fruit Smoothie🍌')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
