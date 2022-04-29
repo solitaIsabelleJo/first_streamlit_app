7bbc7abde9817fd322de164bda4a37bed9f5d86d
solitaIsabelleJo <102659576+solitaIsabelleJo@users.noreply.github.com>
2022-04-29 11:17:28 +0200

    Update streamlit_app.py
    added header

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,10 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
 #API response
+stremlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #streamlit.text(fruityvice_response.json())
 #Normalise json 
